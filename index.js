8380275f9b0be2b33ed127b2e0e7dd1df15696e8
Godwin Bamson <bamsongodwin@gmail.com>
2025-03-22 17:53:19 +0100

    Some changes

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://inventory-management-client-1r19.onrender.com"],
     credentials: true,
   })
 );
